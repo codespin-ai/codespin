e3646eb121bafd7a07203bc43c90e9802b7add52
Jeswin Kumar <jeswinpk@agilehead.com>
2023-10-02 17:23:13 +0530

    Copy codespin default templates during init

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,6 +1,9 @@
 import { CommandResult } from "./CommandResult.js";
 import { promises as fs } from "fs";
+import path from "path";
 import { resolve } from "path";
+import * as url from "url";
+import { join } from "path";
 
 type InitArgs = {
   force?: boolean;
@@ -32,6 +35,26 @@ async function init(args: InitArgs): Promise<CommandResult> {
       JSON.stringify(DEFAULT_JSON_CONTENT, null, 2)
     );
 
+    // Create codespin directories.
+    await createDirectoriesIfNotExist(resolve(currentDir, "codespin"));
+    await createDirectoriesIfNotExist(
+      resolve(currentDir, "codespin/templates")
+    );
+    await createDirectoriesIfNotExist(
+      resolve(currentDir, "codespin/templates/default")
+    );
+
+    // Copy default templates into it.
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const defaultTemplatesDir = join(__filename, "../../../templates/default");
+
+    // Copy all templates into the codespin directory.
+    await copyFiles(
+      defaultTemplatesDir,
+      resolve(currentDir, "codespin/templates/default")
+    );
+
     return { success: true, message: "Initialization completed." };
   } catch (err: any) {
     return {
@@ -47,6 +70,66 @@ async function fileExists(path: string): Promise<boolean> {
     return true;
   } catch (err) {
     return false;
+  }
+}
+
+async function directoryExists(dirPath: string): Promise<boolean> {
+  try {
+    const stats = await fs.stat(dirPath);
+    return stats.isDirectory();
+  } catch (error: any) {
+    if (error.code === "ENOENT") {
+      // File or directory does not exist
+      return false;
+    }
+    throw error; // Rethrow other errors
+  }
+}
+
+async function createDirectoriesIfNotExist(
+  ...dirPaths: string[]
+): Promise<void> {
+  for (const dirPath of dirPaths) {
+    const exists = await directoryExists(dirPath);
+    if (!exists) {
+      await fs.mkdir(dirPath, { recursive: true }); // Using recursive to ensure all nested directories are created
+    }
+  }
+}
+
+async function copyFiles(srcDir: string, destDir: string): Promise<void> {
+  try {
+    // Check if the source and destination directories exist
+    if (!(await fs.stat(srcDir).then((stats) => stats.isDirectory()))) {
+      throw new Error(
+        `Source directory "${srcDir}" doesn't exist or is not a directory.`
+      );
+    }
+
+    if (!(await fs.stat(destDir).then((stats) => stats.isDirectory()))) {
+      throw new Error(
+        `Destination directory "${destDir}" doesn't exist or is not a directory.`
+      );
+    }
+
+    // Read the source directory content
+    const files = await fs.readdir(srcDir);
+
+    // Copy each file from the source directory to the destination directory
+    await Promise.all(
+      files.map(async (file) => {
+        const srcFilePath = path.join(srcDir, file);
+        const destFilePath = path.join(destDir, file);
+
+        const fileStats = await fs.stat(srcFilePath);
+
+        if (fileStats.isFile()) {
+          await fs.copyFile(srcFilePath, destFilePath);
+        }
+      })
+    );
+  } catch (err: any) {
+    console.error(`Failed to copy files: ${err.message}`);
   }
 }
 
