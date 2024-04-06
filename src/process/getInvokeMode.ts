a9a631b1fa22b354b37229a9314fad2d51e8d0ce
Jeswin Kumar <jeswinpk@agilehead.com>
2024-04-06 07:53:42 +0530

    Fix stdin processing

--- src/process/getInvokeMode.ts
+++ src/process/getInvokeMode.ts
@@ -1,15 +1,11 @@
-// Convert file path to file URL
-function pathToFileURL(filePath: string) {
-  return new URL(`file://${filePath}`);
-}
+import path from "path";
+import { fileURLToPath } from "url";
 
-// Function to check if the current module is the main module
 function isMainModule() {
-  // Convert the file path in process.argv[1] to a URL
-  const mainModuleURL = pathToFileURL(process.argv[1] || "").href;
-
-  // Compare the current module's URL with the main module's URL
-  return import.meta.url === mainModuleURL;
+  return (
+    process.argv[1] ===
+    path.resolve(fileURLToPath(import.meta.url), "../../index.js")
+  );
 }
 
 let isModule: "cli" | "module" | undefined = undefined;
