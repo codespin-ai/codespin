a67d8e69358db0d1af1e074f29057d33c96f9f9e
Jeswin Kumar <jeswinpk@agilehead.com>
2024-12-14 14:16:18 +0530

    hasChanges fix

--- src/git/hasUncommittedChanges.ts
+++ src/git/hasUncommittedChanges.ts
@@ -1,18 +1,10 @@
 import { execString } from "../process/execString.js";
-import { getPathRelativeToGitRoot } from "./getPathRelativeToGitRoot.js";
 
 export async function hasUncommittedChanges(
-  filePath: string,
   workingDir: string
 ): Promise<boolean> {
   try {
-    const relativePath = await getPathRelativeToGitRoot(filePath, workingDir);
-    const status = await execString(
-      `git status --porcelain -- ${relativePath}`,
-      workingDir
-    );
-
-    // git status --porcelain returns empty string if there are no changes
+    const status = await execString(`git status --porcelain`, workingDir);
     return status.length > 0;
   } catch (error) {
     return false;
